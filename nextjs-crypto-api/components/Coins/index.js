23aafd167fa68c619dfb1cbc24d3c30f43f52525
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-30 17:19:28 -0500

    more progress

--- nextjs-crypto-api/components/Coins/index.js
+++ nextjs-crypto-api/components/Coins/index.js
@@ -1,26 +1,37 @@
+import styles from "./Coins.module.css";
 
+const Coins = ({
+    name,
+    price,
+    symbol,
+    marketcap,
+    volume,
+    image,
+    priceChange,
+    id
+    }) => {
+        return (
+            <div classNme={styles.coin_container}>
+                <div className="coin_row">
+                    <div className="coin">
+                        <img src={image} alt={name} className="coin_img"/>
+                        <h1 className="coin_h1">{name}</h1>
+                        <p className="coin_symbol">{symbol}</p>
+                    </div>
+                    <div className="coin_data">
+                        <p className="coin_price">${price}</p>
+                        <p className="coin_volume">${volume.toLocaleString()}</p>
+                        {priceChange < 0 ? (
+                            <p className="coin_percent red">{priceChange.toFixed(2)}%</p>
+                        ) : (
+                            <p className="coin_percent green">{priceChange.toFixed(2)}%</p>
+                        )}
 
-const Coins = ({name, price, symbol, marketcap, volume, image, priceChange, id}) => {
-    return (
-        <div classNme="coin_container">
-            <div className="coin_row">
-                <div className="coin">
-                    <img src={image} alt={name} className="coin_img"/>
-                    <h1 className="coin_h1">{name}</h1>
-                    <p className="coin_symbol">{symbol}</p>
-                </div>
-                <div className="coin_data">
-                    <p className="coin_price">${price}</p>
-                    <p className="coin_volume">${volume.toLocaleString()}</p>
-                    {priceChange < 0 ? (
-                        <p className="coin_percent red">{priceChange.toFixed(2)}%</p>
-                    ) : (
-                        <p className="coin_percent green">{priceChange.toFixed(2)}%</p>
-                    )}
+                        <p className="coin_marketcap">Market Cap: ${marketcap.toLocaleString()}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+            );
+        };
 
 export default Coins;
