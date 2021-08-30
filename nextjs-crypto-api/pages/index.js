b80a31acf245633be42a1d6020ce02b771ddee92
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-30 10:47:06 -0500

    adding coins components

--- nextjs-crypto-api/pages/index.js
+++ nextjs-crypto-api/pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head';
 import SearchBar from '../components/SearchBar';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Chart Beast</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <SearchBar />
+      <SearchBar type="text" placeholder="Search"/>
     </div>
   )
 }
