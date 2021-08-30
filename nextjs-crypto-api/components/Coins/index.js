4858c6c04180561d7bfa2834bd610960c4eb07e0
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-30 12:11:18 -0500

    Update index.js

--- nextjs-crypto-api/components/Coins/index.js
+++ nextjs-crypto-api/components/Coins/index.js
@@ -1,10 +1,15 @@
 
 
-const Coins = ({name}) => {
+const Coins = ({name, price, symbol, marketcap, volume, image, priceChange, id}) => {
     return (
-        <div>
-            <p>Hello</p>
-            <h1>{name}</h1>
+        <div classNme="coin_container">
+            <div className="coin_row">
+                <div className="coin">
+                    <img src={image} alt={name} className="coin_img"/>
+                    <h1 className="coin_h1">{name}</h1>
+                    <p className="coin_symbol">{symbol}</p>
+                </div>
+            </div>
         </div>
     );
 };
